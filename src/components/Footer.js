3f16c8fa5d3309d60517b2f4d1628f4f770919ef
uicheol <suc2566@naver.com>
2023-02-28 23:35:23 +0900

    1.6.footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import Link from "@mui/material/Link";
 
 const Footer = () => {
   return (
@@ -31,10 +32,32 @@ const Footer = () => {
               justifyContent: "space-between",
             }}
           >
-            <Box>네이버카페</Box>
-            <Box>방재기상정보시스템</Box>
             <Box>
-              <GitHubIcon />
+              <Link
+                href="https://cafe.naver.com/helloweather"
+                underline="none"
+                color="inherit"
+              >
+                네이버카페
+              </Link>
+            </Box>
+            <Box>
+              <Link
+                href="https://afso.kma.go.kr/afsOut/rsp/ptl/login.jsp"
+                underline="none"
+                color="inherit"
+              >
+                방재기상정보시스템
+              </Link>
+            </Box>
+            <Box>
+              <Link
+                href="https://github.com/mini-apple/helloweather-v5"
+                underline="none"
+                color="inherit"
+              >
+                <GitHubIcon />
+              </Link>
             </Box>
           </Box>
         </Box>
